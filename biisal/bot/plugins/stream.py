7162c660ead54f5dfd509850ef53b6cab8016d47
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2024-02-29 15:16:43 +0530

    Update stream.py

--- biisal/bot/plugins/stream.py
+++ biisal/bot/plugins/stream.py
@@ -104,7 +104,8 @@ async def private_receive_handler(c: Client, m: Message):
         online_link = f"{Var.URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
 #        stream_link = get_shortlink(stream_links) 
 #        online_link = get_shortlink(online_links)
-        tutorial = f"{Var.TUTORIAL_URL}"
+         tutorial = f"{Var.UPDATES_GROUP}"
+    
 
         await log_msg.reply_text(text=f"**RᴇQᴜᴇꜱᴛᴇᴅ ʙʏ :** [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n**Uꜱᴇʀ ɪᴅ :** `{m.from_user.id}`\n**Stream ʟɪɴᴋ :** {stream_link}", disable_web_page_preview=True,  quote=True)
         await m.reply_text(
@@ -112,8 +113,8 @@ async def private_receive_handler(c: Client, m: Message):
             quote=True,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("sᴛʀᴇᴀᴍ 🔺", url=stream_link), #Stream Link
-                                               [InlineKeyboardButton('ᴅᴏᴡɴʟᴏᴀᴅ 🔻', url=online_link)], #Download Link
-                                            [InlineKeyboardButton("ᴊᴏɪɴ ᴏᴜʀ ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ", url=f"https://t.me/{Var.UPDATES_GROUP}"]]) #Tutorial Video
+                                               InlineKeyboardButton('ᴅᴏᴡɴʟᴏᴀᴅ 🔻', url=online_link)], #Download Link
+                                            [InlineKeyboardButton("ᴊᴏɪɴ ᴏᴜʀ ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ", url=tutorial)]]) #Tutorial Video
         )
     except FloodWait as e:
         print(f"Sleeping for {str(e.x)}s")
