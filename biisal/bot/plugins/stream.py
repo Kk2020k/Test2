bea7fadd2d2f9f5f8734c9edf1a8181f9890b391
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2024-11-08 21:56:18 +0530

    Update stream.py

--- biisal/bot/plugins/stream.py
+++ biisal/bot/plugins/stream.py
@@ -89,19 +89,19 @@ async def private_receive_handler(c: Client, m: Message):
     try:
         log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
         stream_link = f"{Var.URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
-        stream_short = await.get_shortlink(f"{Var.URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}")
+        s = await.get_shortlink(f"{Var.URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}")
        
     #    online_link = f"{Var.URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
         online_link = f"https://tnshort.net?api={Var.API}&url={Var.URL}{str(log_msg.message_id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
-        online_short = await.get_shortlink(online_link)
+        d = await.get_shortlink(online_link)
        
         await log_msg.reply_text(text=f"**RᴇQᴜᴇꜱᴛᴇᴅ ʙʏ :** [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n**Uꜱᴇʀ ɪᴅ :** `{m.from_user.id}`\n**Stream ʟɪɴᴋ :** {stream_link}", disable_web_page_preview=True,  quote=True)
         await m.reply_text(
-            text=msg_text.format(get_name(log_msg), humanbytes(get_media_file_size(m)), online_link, stream_short),
+            text=msg_text.format(get_name(log_msg), humanbytes(get_media_file_size(m)), online_link, s),
             quote=True,
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("sᴛʀᴇᴀᴍ 🔺", url=stream_short), #Stream Link
-                                               InlineKeyboardButton('ᴅᴏᴡɴʟᴏᴀᴅ 🔻', url=online_short)]]) #Download Link
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("sᴛʀᴇᴀᴍ 🔺", url=s), #Stream Link
+                                               InlineKeyboardButton('ᴅᴏᴡɴʟᴏᴀᴅ 🔻', url=d)]]) #Download Link
         )
     except FloodWait as e:
         print(f"Sleeping for {str(e.x)}s")
